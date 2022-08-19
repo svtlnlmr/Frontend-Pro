369ce5d6c4761903df129b740cdc1e5db3a18bb2
Svitlana Lymar <svtln.lmr.ua@gmail.com>
2022-08-19 10:31:47 +0300

    homework 15 -- empty version

---
+++ Lesson-Pro-15-HW-empty-version/js/js.js
@@ -0,0 +1,70 @@
+const container = document.querySelector('.container');
+const filters = {
+    sex: {
+        male: { gender: 'Male', input: document.getElementById('male') },
+        female: { gender: 'Female', input: document.getElementById('female') },
+    },
+    status: {
+        alive: { status: 'Alive', input: document.getElementById('alive') },
+        dead: { status: 'Dead', input: document.getElementById('dead') },
+    },
+};
+renderPage();
+
+async function renderPage() {
+    const characters = await fetchCharacters();
+    container.innerHTML = renderCharacters(characters);
+    Object.values(filters.sex).map(async ({ gender, input }) => {
+        input.addEventListener('change', () => {
+            const filteredCharacters = !input.checked
+                ? characters
+                : characters.filter((character) => character.gender === gender);
+            container.innerHTML = renderCharacters(filteredCharacters);
+        });
+    });
+
+    Object.values(filters.status).map(async ({ status, input }) => {
+        input.addEventListener('change', () => {
+            const filteredCharacters = !input.checked
+                ? characters
+                : characters.filter((character) => character.status === status);
+            container.innerHTML = renderCharacters(filteredCharacters);
+        });
+    });
+}
+
+async function fetchCharacters() {
+    const response = await fetch(
+        'https://rickandmortyapi.com/api/character/'
+    ).then((res) => res.json());
+    return response.results;
+}
+
+function renderCharacters(characters) {
+    return characters
+        .map(
+            ({ name, species, status, location: { name: locationName }, image }) => {
+                return `
+            <div class="card">
+                <div class="card-info">
+                    <div class="title">
+                        <h1>${name}</h1>
+                        <div class="status">
+                            <div class="live-status ${
+                    'Dead' === status ? 'dead' : ''
+                }"></div>
+                            <p>${species} -- ${status}</p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <p>${locationName}</p>
+                    </div>
+                </div>
+                <div class="card-image">
+                    <img src="${image}" alt="${name}">
+                </div>
+            </div>`;
+            }
+        )
+        .join('');
+}
