6f081004448abf5475a4425111000bd0bd42c2cb
Svitlana Lymar <svtln.lmr.ua@gmail.com>
2022-09-10 20:07:51 +0300

    homework-21

--- Lesson-Pro-21-hw/my-app/src/components/HelloPage.js
+++ Lesson-Pro-21-hw/my-app/src/components/HelloPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Image from "./Image";
+import { Clock } from "./Clock";
 
 class HelloPage extends React.Component {
     render() {
         return (
             <div className="Hello-Page">
-                <Image/>
-                <div>{this.props.text}</div>
-                <div>Time now: {this.props.date.toLocaleTimeString()}</div>
+                <Image />
+                <h2>{this.props.text}</h2>
+                <Clock />
             </div>
         );
     }
